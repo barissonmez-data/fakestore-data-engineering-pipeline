12e747d84f0ca477a67b8a25dd8227d5c544d798
Baris Sonmez <brsson06@hotmail.com>
2026-08-27 19:08:45 +0200

    add null and duplicate quality checks

--- sql/03_data_quality_checks.sql
+++ sql/03_data_quality_checks.sql
@@ -1,4 +1,18 @@
+-- check for dupicate rows by product_key , user_key , quantity
 select product_key, user_key, quantity, count(*)
 from fakestore_warehouse.facts_a
 group by product_key, user_key, quantity
 having count(*) > 1;
+
+
+
+
+-- we checked missing product_keys 
+select * from fakestore_warehouse.facts_a
+where product_key is null;
+
+
+
+-- checeked  for missing user_keys after join
+select * from fakestore_warehouse.facts_a
+where user_key is null;
