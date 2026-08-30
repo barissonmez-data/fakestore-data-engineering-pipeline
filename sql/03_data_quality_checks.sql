6075a307538ad8906e3f3d771d8aa8aa2aa717c3
Baris Sonmez <brsson06@hotmail.com>
2026-08-30 19:43:51 +0200

    check for duplicate

--- sql/03_data_quality_checks.sql
+++ sql/03_data_quality_checks.sql
@@ -16,3 +16,21 @@ where product_key is null;
 -- checeked  for missing user_keys after join
 select * from fakestore_warehouse.facts_a
 where user_key is null;
+
+
+
+
+select stg_products.id , count(*) from fakestore_project.stg_products group by stg_products.id
+having count(*) >1
+
+select price,category, count(*) from fakestore_project.stg_products 
+group by category ,price
+HAVING count(*)>1
+
+
+
+
+
+select id,price,category, count(*) from fakestore_project.stg_products 
+group by id,category ,price
+HAVING count(*)>1
